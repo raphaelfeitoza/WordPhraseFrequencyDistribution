6e6b61f859c6d0e1a08b433daf0a6971d1ca5c73
Raphael Lopes Feitoza <rlfeitoza@outlook.com>
2017-07-18 22:58:46 -0400

    Code

---
+++ Counting/Counting/CountingWords.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Counting
+{
+    public class CountingWords
+    {
+        public Dictionary<string, int> CountWordPhraseFrequency(string[] phrases)
+        {
+            var splitedPhrases = phrases.Select(w => w.Split(' '));
+
+            var words = splitedPhrases.SelectMany(w => w).ToList();
+            
+            List<string> listFragment = ExtractFragments(splitedPhrases);
+
+            words.AddRange(listFragment);
+
+            Dictionary<string, int> wordsCountFrequency = ComputeWordPhraseFrequencyDistribution(words);
+
+            return wordsCountFrequency;
+        }
+
+        private static List<string> ExtractFragments(IEnumerable<string[]> splitedPhrases)
+        {
+            var listFragment = new List<string>();
+
+            foreach (var phrase in splitedPhrases)
+            {
+                for (int i = 0; i < phrase.Count(); i++)
+                {
+                    var firstPart = phrase[i];
+                    var secondPart = string.Empty;
+
+                    for (int j = i + 1; j < phrase.Count(); j++)
+                    {
+                        secondPart = string.IsNullOrEmpty(secondPart) ? phrase[j] : string.Join(" ", secondPart, phrase[j]);
+
+                        listFragment.Add(string.Join(" ", firstPart, secondPart));
+                    }
+                }
+            }
+
+            return listFragment;
+        }
+
+        private static Dictionary<string, int> ComputeWordPhraseFrequencyDistribution(List<string> words)
+        {
+            words.Sort();
+            var wordsCount = new Dictionary<string, int>();
+            var lastCountedWord = string.Empty;
+            foreach (var word in words)
+            {
+                if (word == lastCountedWord)
+                    wordsCount[word]++;
+                else
+                    wordsCount.Add(word, 1);
+
+                lastCountedWord = word;
+            }
+
+            return wordsCount;
+        }
+    }
+}
---
+++ Counting/CountingTest/CountingWordsTest.cs
@@ -0,0 +1,50 @@
+﻿using Counting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CountingTest
+{
+    [TestClass]
+    public class CountingWordsTest
+    {
+        private readonly string[] _phrases = new[] { "One Two Three", "Two Three Four", "Three Four Five" };
+
+        [TestMethod]
+        public void CountWordPhraseFrequency_OnlyWordCounts_WordsCounts()
+        {          
+            var counter = new CountingWords();
+            var wordFrequency =  counter.CountWordPhraseFrequency(_phrases);
+
+            Assert.AreEqual(1, wordFrequency["One"]);
+
+            Assert.AreEqual(2, wordFrequency["Two"]);
+
+            Assert.AreEqual(3, wordFrequency["Three"]);
+
+            Assert.AreEqual(2, wordFrequency["Four"]);
+
+            Assert.AreEqual(1, wordFrequency["Five"]);
+        }
+
+        [TestMethod]
+        public void CountWordPhraseFrequency_FragmentsCount_FrequenciesCounts()
+        {
+            var counter = new CountingWords();
+            var wordFrequency = counter.CountWordPhraseFrequency(_phrases);
+
+            Assert.AreEqual(1, wordFrequency["One Two"]);
+
+            Assert.AreEqual(2, wordFrequency["Two Three"]);
+
+            Assert.AreEqual(2, wordFrequency["Three Four"]);
+
+            Assert.AreEqual(1, wordFrequency["Four Five"]);
+
+            Assert.AreEqual(1, wordFrequency["One Two Three"]);
+
+            Assert.AreEqual(1, wordFrequency["Two Three Four"]);
+
+            Assert.AreEqual(1, wordFrequency["Three Four Five"]);
+        }
+
+    }
+}
